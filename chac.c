573586442e7b27ed67423f72c2502838b3d0517a
Hubert Liu <hubertliu100@gmail.com>
2025-03-26 20:52:59 -0400

    initial commit

---
+++ chac.c
@@ -0,0 +1,103 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define QR(a, b, c, d) (             \
+	a += b, d ^= a, d = ROTL(d, 16), \
+	c += d, b ^= c, b = ROTL(b, 12), \
+	a += b, d ^= a, d = ROTL(d,  8), \
+	c += d, b ^= c, b = ROTL(b,  7))
+#define ROUNDS 20
+
+// binary representation of "ChaC"
+#define CONST 0b01000011011010000110000101000011
+
+void keystream(uint32_t out[8], uint32_t const in[8]) {
+
+    uint32_t x[16];
+
+    // Copy input block to output
+    for (int i = 0; i < 8; i++) {
+        x[i] = in[i];
+    }
+
+    for (int i = 0; i < ROUNDS; i+=2) {
+        // Columns
+        QR(x[0], x[1], x[2], x[3]);
+        QR(x[4], x[5], x[6], x[7]);
+
+        // Diagonals
+        QR(x[0], x[5], x[2], x[7]);
+        QR(x[1], x[6], x[3], x[4]);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        out[i] = x[i] ^ in[i];
+
+    }
+
+    // TODO: Generate the next three 256-bit keystreams
+}
+
+void print_block(uint32_t const in[8]) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 32; j++) {
+            printf("%c", ((in[i] >> (31-j)) & 1) ? 'X' : '.');
+        }
+        printf(" ");
+        if (i%4 == 3) { printf("\n"); }
+    }
+}
+
+void copy_block(uint32_t dest[8], uint32_t const src[8]) {
+    for (int i = 0; i < 8; i++) {
+        dest[i] = src[i];
+    }
+}
+
+int block_diffs(uint32_t a[8], uint32_t b[8]) {
+    int diffs = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 32; j++) {
+            diffs += ((a[i] >> j) & 1) ^ ((b[i] >> j) & 1);
+        }
+    }
+    return diffs;
+}
+
+int main() {
+    /*uint32_t key[4], nonce[2];*/
+    /*uint32_t count;*/
+    uint32_t block[8];
+
+
+    uint32_t key[4] = { 0, 0, 0, 0 };
+    uint32_t nonce[2] = { 0, 0 };
+    uint32_t count = 0;
+
+    block[0] = key[0];
+    block[1] = key[1];
+    block[2] = key[2];
+    block[3] = key[3];
+
+    block[4] = count;
+    block[5] = nonce[0];
+    block[6] = nonce[1];
+    block[7] = CONST;
+
+    print_block(block);
+
+    printf("-----------------------------------------------------------------------------------------------------------------------------------\n");
+
+    uint32_t res[8], last_res[8];
+    copy_block(last_res, block);
+
+    for (int i = 0; i < 10; i++) {
+        keystream(res, block);
+        print_block(res);
+        printf("%d\n\n", block_diffs(res, last_res));
+        copy_block(last_res, res);
+        block[4]++; // count
+    }
+
+}
